dff19332e781d84b329420e49984c5090edf6652
Lorraine Frontelli <lorrainefrontelli@gmail.com>
2026-08-12 12:10:00 -0300

    remove acessor novo do antigo

--- migracao_fastAPI/app/agentes/financeiro/financeiro_app.py
+++
@@ -1,9 +0,0 @@
-from langchain.agents import create_agent
-
-from ..llms import specialist_llm
-from .financeiro_prompts import FINANCIAL_PROMPT
-from .tools import TOOLS
-
-financial_app = create_agent(
-    model=specialist_llm, system_prompt=FINANCIAL_PROMPT, tools=TOOLS
-)
--- migracao_fastAPI/app/agentes/financeiro/tools/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .resolve_type_id import resolve_type_id
-from .resolve_category_id import resolve_category_id
